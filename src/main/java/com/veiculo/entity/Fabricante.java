523890369c31e37b8a9358759613691d25b9b0fe
RAQUEL PEREIRA BEZERRA <raquel.bezerra@alunos.sc.senac.br>
2025-08-05 21:46:33 -0300

    Primeiro Commit

---
+++ src/main/java/com/veiculo/entity/Fabricante.java
@@ -0,0 +1,56 @@
+package com.veiculo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+
+public class Fabricante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String nome;
+
+    @Column(nullable = false, length = 50)
+    private String paisOrigem;
+
+
+    public Fabricante(){
+
+    }
+
+    public Fabricante(Long id, String nome, String paisOrigem) {
+        this.id = id;
+        this.nome = nome;
+        this.paisOrigem = paisOrigem;
+    }
+    
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getPaisOrigem() {
+        return paisOrigem;
+    }
+    public void setPaisOrigem(String paisOrigem) {
+        this.paisOrigem = paisOrigem;
+    }
+
+
+}
