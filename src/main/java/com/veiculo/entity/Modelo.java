00b5cd73519df518b84b1472ed1a02b225a6b508
RAQUEL PEREIRA BEZERRA <raquel.bezerra@alunos.sc.senac.br>
2025-08-11 21:28:57 -0300

    Criação das classe Modelo e Veículo

---
+++ src/main/java/com/veiculo/entity/Modelo.java
@@ -0,0 +1,66 @@
+package com.veiculo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Modelo {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String nome;
+    
+    @ManyToOne
+    @JoinColumn(name = "fabricante_id", nullable = false)
+    private Fabricante fabricante;
+
+    public Modelo(){
+
+    }
+
+    public Modelo(Long id, String nome, Fabricante fabricante) {
+        this.id = id;
+        this.nome = nome;
+        this.fabricante = fabricante;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Fabricante getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(Fabricante fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    
+
+    
+
+    
+
+
+}
